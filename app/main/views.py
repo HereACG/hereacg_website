4e82c589caf76159a468133d04acc32e7bfd44e3
thislight <thislight@users.noreply.github.com>
2016-09-30 22:04:08 +0800

    Update views for main: ADD index() for '/'.

--- app/main/views.py
+++ app/main/views.py
@@ -1,1 +1,9 @@
+from . import main
+from Flask import render_template, url_for, request
 
+@main.route("/")
+def index():
+    arg = {
+    'title': "主页"
+    }
+    return render_template('main/index.html',arg=arg),
