5abff1ad9543c047c2136f2b9f7a11aa75c7be78
thislight <thislight@users.noreply.github.com>
2016-09-30 23:20:08 +0800

    A commit before go to bed

--- app/auth/views.py
+++ app/auth/views.py
@@ -3,3 +3,8 @@ from flask import render_template, session, redirect, url_for
 
 from . import auth
 from .forms import LoginForm
+from .helper import apiv1_prefix
+
+@auth.route(apiv1_prefix('/register/<username>'))
+def register():
+    pass
---
+++ app/email.py
@@ -0,0 +1,9 @@
+import asyncio
+import stmplib
+from .helper import Request
+
+class EmailRequest(Request):
+    pass
+
+async def send(request):
+    pass
