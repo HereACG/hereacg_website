e7b3b0fe13dced5787d178366668e86b7768a62b
thislight <thislight@users.noreply.github.com>
2016-10-01 21:14:52 +0800

    UPDATE auth views&helper auth/helper.py:  +class NoAPIKeyException(Exception)  +class APIKeyNeedFunction(object)  +def apikey_need(egg) auth/views.py:  +@auth.route(apiv1_prefix('/manager/getToken'),method=['POST'])   def get_token()
    Signed-off-by: thislight <thislight@users.noreply.github.com>

--- app/auth/helper.py
+++ app/auth/helper.py
@@ -2,5 +2,32 @@
 Some helper function for package auth
 """
 
+class NoAPIKeyException(Exception):
+    pass
+
+class APIKeyNeedFunction(object):
+    def __init__(egg,app):
+        self._egg = egg
+        self._app = app
+
+    def apikey(self,r):
+        apikey = r.args.get('apikey',None)
+        if apikey != None:
+            return apikey
+        else:
+            raise NoAPIKeyException()
+
+    def __call__(self,*args,**kargs):
+        try:
+            self._egg(self.apikey(request),*args,**kargs)
+        except NoAPIKeyException as e:
+            self._app.logger.error("A ERROR Happened:"+e)
+
+
+
 def apiv1_prefix(s):
     return '/api/v1' + s
+
+
+def apikey_need(egg):
+    pass
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,10 +1,10 @@
 from datetime import datetime
-from flask import render_template, session, redirect, url_for
+from flask import request,render_template, session, redirect, url_for
 
 from . import auth
 from .forms import LoginForm
 from .helper import apiv1_prefix
 
-@auth.route(apiv1_prefix('/register/<username>'))
-def register():
+@auth.route(apiv1_prefix('/manager/getToken'),method=['POST'])
+def get_token():
     pass
